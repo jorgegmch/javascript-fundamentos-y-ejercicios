03996dc3b7e5d6a36ebf72bd2871f4332f48e623
jorgegmch <gomezjorge.ch@gmail.com>
2025-12-04 20:56:19 -0500

    feat: se agrega tema sobre DOM

---
+++ 12-DOM/propiedades/01-dom-propiedades.js
@@ -0,0 +1,18 @@
+// 1. classList.length
+
+const miDiv = document.getElementById("miElemento");
+console.log("Cantidad de clases del div: ", miDiv.classList.length);
+
+// 2 classList y classList.value
+console.log("Clases del div: ", miDiv.classList.value);
+
+// 3. classList.item(n)
+console.log("Segunda clase del div: ", miDiv.classList.item(1));
+
+// 4. classList.contains - verifica si una clase está en la lista de clases
+console.log("¿Está la clase 'clase3' en el div?: ", miDiv.classList.contains("clase3"));
+
+
+
+
+
